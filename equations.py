150433c84a9648e066629dac550380596f21dc66
Jasmine Pattany <pattanyj@gmail.comcd>
2022-02-18 17:18:03 +0100

    feb 18 2022

--- equations.py
+++ equations.py
@@ -4,53 +4,77 @@ Created on Sat Feb 12 16:54:12 2022
 
 @author: patta
 """
-#%% EXAMPLE
 
-def logistic(x, start, K, x_peak, r):
-    """
-    Logistic model
-    
-    This function runs a logistic model.
-    
-    Args:
-        x (array_like): The control variable as a sequence of numeric values \
-        in a list or a numpy array.
-        start (float): The initial value of the return variable.
-        K (float): The carrying capacity.
-        x_peak (float): The x-value with the steepest growth.
-        r (float): The growth rate.
-        
-    Returns:
-        array_like: A numpy array or a single floating-point number with \
-        the return variable.
-    """
-    
-    if isinstance(x, list):
-        x = np.array(x)
-    return start + K / (1 + np.exp(r * (x_peak-x)))
 
 #%%
 # imports
 
 import pandas as pd
 import numpy as np
-import matplotlib as plt
 import math
-# from explore_data import 
 
+# data
 BACI_df = pd.read_csv('./Data/BACI_HS17_Y2017_V202102.csv')
-BACI_cobalt = BACI_df.loc[BACI_df['k'].isin([260500,282200,810520,810530,810590,])]
+cobalt_products = [260500,282200,810520,810530,810590]
+BACI_cobalt = BACI_df.loc[BACI_df['k'].isin(cobalt_products)]
 
-def calculations(ISO_importer,ISO_exporter):
-    
-    Tij = BACI_cobalt['q'].loc[BACI_cobalt['i'].isin([ISO_importer]) & BACI_cobalt['j'].isin([ISO_exporter])].sum()
-    Ti = BACI_cobalt['q'].loc[BACI_cobalt['i'].isin([ISO_importer])].sum()
-    Tj = BACI_cobalt['q'].loc[BACI_cobalt['j'].isin([ISO_exporter])].sum()
-    T = Ti + Tj
-    
-    efficiency = (Tij/T)*math.log((Tij*T)/(Ti*Tj))
-    redundancy = (Tij/T)*math.log((Tij**2)/(Ti*Tj))
-    alpha = efficiency/(redundancy+efficiency)
-    theoretical_resilience = (-alpha)*math.log(alpha)
+test = BACI_cobalt.groupby(['i','k']).sum()
+#%%
 
-    return efficiency,redundancy,alpha,theoretical_resilience
+def calculations(cobalt_products):
+
+    calculations_df = pd.DataFrame(columns=['product','efficiency','redundancy','alpha',
+                                        'theoretical_resilience'])
+
+    for product in cobalt_products:  
+        
+        gb_exporter = BACI_cobalt.groupby(['i','k']).sum()
+        gb_importer = BACI_cobalt.groupby(['j','k']).sum()
+        
+        Tij = BACI_cobalt['q'].loc[BACI_cobalt['k'].isin([product])].sum()
+        Ti = gb_exporter['q'].loc[gb_exporter['k'].isin([product])].sum()
+        Tj = gb_importer['q'].loc[gb_importer['k'].isin([product])].sum()
+        T = Ti + Tj
+ 
+        efficiency = (Tij/T)*math.log((Tij*T)/(Ti*Tj))
+        redundancy = (Tij/T)*math.log((Tij**2)/(Ti*Tj))
+        alpha = efficiency/(redundancy+efficiency)
+        theoretical_resilience = (-alpha)*math.log(alpha)
+        
+        to_append = [product,efficiency,redundancy,alpha,theoretical_resilience]
+        series = pd.Series(to_append, index = calculations_df.columns)
+        calculations_df = calculations_df.append(series, ignore_index=True)
+        
+    return calculations_df
+
+#%%
+        
+    
+    
+# def calculations(ISO_importer,ISO_exporter):   # https://journals.plos.org/plosone/article?id=10.1371/journal.pone.0171184 for calc
+   
+    """ DO THIS
+    Calculations model
+    
+    This function runs a model.
+    
+    Args:
+        BACI_cobalt (DataFrame): explain here.
+
+    Returns:
+        array_like: explain here.
+    """
+    # per k (product) needs to be calc then groupby sums and calc eff, red, alpha, ...
+    
+    
+    # Tij = BACI_cobalt['q'].loc[BACI_cobalt['i'].isin([ISO_importer]) & BACI_cobalt['j'].isin([ISO_exporter])].sum()
+    # Ti = BACI_cobalt['q'].loc[BACI_cobalt['i'].isin([ISO_importer])].sum()
+    # Tj = BACI_cobalt['q'].loc[BACI_cobalt['j'].isin([ISO_exporter])].sum()
+    # T = Ti + Tj
+    
+    # efficiency = (Tij/T)*math.log((Tij*T)/(Ti*Tj))
+    # redundancy = (Tij/T)*math.log((Tij**2)/(Ti*Tj))
+    # alpha = efficiency/(redundancy+efficiency)
+    # theoretical_resilience = (-alpha)*math.log(alpha)
+
+    # return efficiency,redundancy,alpha,theoretical_resilience
--- explore_data.py
+++ explore_data.py
@@ -7,6 +7,10 @@ This is a temporary script file.
 # imports
 import json
 import pandas as pd
+import numpy as np
+# import math
+from pyvis.network import Network
+import networkx as nx
 
 #%% PANORAMA Data
 # Opening JSON file
@@ -20,10 +24,7 @@ dim_code = json.load(dim_code)
 elem = json.load(elem)
 extensions = json.load(extensions)
 
-#%% Import BACI Data
-BACI_df = pd.read_csv('./Data/BACI_HS17_Y2017_V202102.csv')
-
-#%% Creating DataFrames 
+# Creating DataFrames 
 # Labels
 region = dim_code['label']['reg']
 product = dim_code['label']['pro']
@@ -33,7 +34,7 @@ industry = dim_code['label']['ind']
 supply_values = elem['Co']['sup']['val']
 supply_position = elem['Co']['sup']['pos']
 
-# Collecting labels for supply
+# Collecting labels for supply - can combine to one for loop also (for later)
 supply_region = []
 for i in supply_position[0]:
     supply_region.append(region[i])   
@@ -50,25 +51,74 @@ for i in supply_position[2]:
 list_tuples = list(zip(supply_region,supply_product,supply_industry,supply_values))    # zip together list values
 supply_df = pd.DataFrame(list_tuples, columns=['Region','Product','Industry','Value (tons)'])   # create df
 
+#%% BACI Data
+# import BACI data for 2017
+BACI_df = pd.read_csv('./Data/BACI_HS17_Y2017_V202102.csv')
+country_code_df = pd.read_csv('./Data/country_code.csv',encoding='unicode_escape')
 
-#%% Calculating alpha
-BACI_cobalt = BACI_df.loc[BACI_df['k'].isin([260500,282200,810520,810530,810590,])]
-# cobalt_china =  BACI_cobalt.loc[BACI_cobalt['j'].isin([156])]
-# belgium_china = BACI_cobalt.loc[BACI_cobalt['i'].isin([56]) & BACI_cobalt['j'].isin([156])]
-# tons_bc = belgium_china['q'].sum()
+# Calculating alpha
+# filter by product
+product = 260500    # Cobalt ores and concentrates
+BACI_product = BACI_df.loc[BACI_df['k'].isin([product])].dropna()
 
-from equations import calculations
+#group by country
+gb_exporter = BACI_product.groupby('i').sum()
+gb_importer = BACI_product.groupby('j').sum()
 
-belg_chi = calculations(56,156) #theoretical resilience just under 0.37 therefore slightly redundant
+# calculations https://journals.plos.org/plosone/article?id=10.1371/journal.pone.0171184
+Tij = BACI_product['q']
+Ti = gb_exporter['q']
+Tj = gb_importer['q']
+T = Ti.sum() + Tj.sum()
 
-#%% PLOT DATA
+efficiency = (Tij/T).sum()*np.log((Tij*T).sum()/(Ti*Tj).sum())
+redundancy = (Tij/T).sum()*np.log((Tij**2).sum()/(Ti*Tj).sum())
+alpha = efficiency/(redundancy+efficiency)
+theoretical_resilience = (-alpha)*np.log(alpha)
 
-from pyvis.network import Network 
+# all_calc = [efficiency, redundancy, alpha]
 
+
+
+
+
+
+
+
+
+
+
+#%% Trying Pyvis
 g = Network()
-g.add_nodes([1,2,3], value=[10, 100, 400],
-                         title=['I am node 1', 'node 2 here', 'and im node 3'],
-                         x=[21.4, 54.2, 11.2],
-                         y=[100.2, 23.54, 32.1],
-                         label=['NODE 1', 'NODE 2', 'NODE 3'],
-                         color=['#00ff1e', '#162347', '#dd4b39'])
+#plotting exporters
+exporter_codes= BACI_product['i'].unique()
+exporter_names = []
+
+for i in exporter_codes:
+    temp = country_code_df.query('country_code' == i)['country_name_full']
+    exporter_names.append(temp)
+
+
+g.add_nodes([1,2,3,4,5,6], value=gb_exporter['q'], 
+            title=[BACI_product['i'].unique()], label='exporters')
+
+nt = Network('500px', '500px')
+
+# populates the nodes and edges data structures
+nt.from_nx(g)
+nt.show('g_plot.html')
+
+#%%
+nx_graph = nx.cycle_graph(10)
+nx_graph.nodes[1]['title'] = 'Number 1'
+nx_graph.nodes[1]['group'] = 1
+nx_graph.nodes[3]['title'] = 'I belong to a different group!'
+nx_graph.nodes[3]['group'] = 10
+nx_graph.add_node(20, size=20, title='couple', group=2)
+nx_graph.add_node(21, size=15, title='couple', group=2)
+nx_graph.add_edge(20, 21, weight=5)
+nx_graph.add_node(25, size=25, label='lonely', title='lonely node', group=3)
+nt = Network('500px', '500px')
+# populates the nodes and edges data structures
+nt.from_nx(nx_graph)
+nt.show('nx.html')
