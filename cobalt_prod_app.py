836e39c0dac6aa5bf10bcab1fac01857552d2d04
Jasmine Pattany <pattanyj@gmail.comcd>
2022-08-17 01:52:58 +0200

    Update cobalt_prod_app.py

--- cobalt_prod_app.py
+++ cobalt_prod_app.py
@@ -24,7 +24,7 @@ import matplotlib
 
 st.subheader('Part of Jasmine Pattany thesis project')
 # st.write("check out this [link](https://share.streamlit.io/mesmith027/streamlit_webapps/main/MC_pi/streamlit_app.py)")
-st.caption('T omessage any comments or questions please click','<a href="mailto:jpattany@gmail.com">here!</a>', unsafe_allow_html=True)
+st.caption('<a href="mailto:jpattany@gmail.com">For any comments or questions please click here.</a>', unsafe_allow_html=True)
 # st.sidebar.markdown('<a href="mailto:jpattany@gmail.com">Contact us !</a>', unsafe_allow_html=True)
 
 
