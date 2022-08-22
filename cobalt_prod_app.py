3b70b9c6f2b2999cc533979361e1939dbf404fe9
Jasmine Pattany <pattanyj@gmail.comcd>
2022-08-22 16:37:55 +0200

    Update cobalt_prod_app.py

--- cobalt_prod_app.py
+++ cobalt_prod_app.py
@@ -1868,7 +1868,7 @@ else:
             
             
             for index, row in df_select.iterrows():
-                nt.add_edge(row['country_code_x'], row['country_code_y'], width=5*(row['q_total'])/(df_select['q_total'].sum()))
+                nt.add_edge(row['country_code_x'], row['country_code_y'], width=20*(row['q_total'])/(df_select['q_total'].sum()))
             
             # Generate network with specific layout settings
             nt.repulsion(node_distance=300, central_gravity=0.01,
